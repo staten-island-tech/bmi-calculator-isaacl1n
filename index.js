646b6d0b188a8e3004abce15b0d16bdba5ec3f53
isaacl81 <isaaclin2007@gmail.com>
2023-10-05 08:45:51 -0400

    function

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 console.log("connected");
-function GetBMI(h,w) {
-    return 20;
+var bmiCalc = prompt("")
+
+function getBMI(h,w) {
+    ;
 }
-function underweight(x)
-    if(x <==  && x>== )
+
+function check(x)
+    if(x >= 18.5 && x < 25) {
+            console.log("normal"); 
+        }
+        else if(x < 18.5) {
+            console.log("underweight"); 
+        }
+        else if(x >= 25 && x < 29.9) {
+            console.log("overweight"); 
+        }
+        else if(x >= 30) {
+            console.log("obese"); 
+        }
+        else {
+            console.log("error");
+        }
 
 let x = getBMI(h,w);
     check(x)
