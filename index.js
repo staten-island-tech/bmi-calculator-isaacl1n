a8e15426e194404c22b2fbe06bb98b41f82a6662
isaacl81 <isaaclin2007@gmail.com>
2023-10-06 08:59:03 -0400

    final

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 console.log("connected");
 
+let displayName = prompt("Please enter your name")
+    console.log("Name: " + displayName)
 var m = prompt("Enter your weight in kilograms");
-var h = prompt("Enter your height in centimeters")
+var h = prompt("Enter your height in centimeters");
 
 function getBMI(h,m) {
     return (m / ((h / 100) * (h / 100)));
 }
 function check(x) {
+    console.log("BMI: " + x)
     if(x >= 18.5 && x < 25) {
             console.log("normal")
-            alert("Your body mass index is " + x + "." + "\nYou are of normal weight."); 
+            alert("Name: " + displayName + "\nYour body mass index is " + x + "." + "\nYou are of normal weight."); 
         }
         else if(x < 18.5) {
             console.log("underweight")
-            alert("Your body mass index is " + x + "." + "\nYou are underweight."); 
+            alert("Name: " + displayName + "\nYour body mass index is " + x + "." + "\nYou are underweight."); 
         }
         else if(x >= 25 && x < 29.9) {
             console.log("overweight")
-            alert("Your body mass index is " + x + "." + "\nYou are overweight."); 
+            alert("Name: " + displayName + "\nYour body mass index is " + x + "." + "\nYou are overweight."); 
         }
         else if(x >= 30) {
             console.log("obese")
-            alert("Your body mass index is " + x + "." + "\nYou are obese."); 
+            alert("Name: " + displayName + "\nYour body mass index is " + x + "." + "\nYou are obese."); 
         }
         else {
             console.log("error")
-            alert("You've encountered an error. Please make sure your inputs are valid.");
+            alert("Name: " + displayName + "You've encountered an error. Please make sure your inputs are valid.");
         }
     }
 
